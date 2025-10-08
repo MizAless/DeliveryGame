9d56f688e1bc28f747b6d207dfc8415a03adff53
Mizaless <44200635+MizAless@users.noreply.github.com>
2025-10-08 16:24:32 +0300

    Добавлен Updater

---
+++ Assets/_Game/Scripts/Updater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Updater : MonoBehaviour
+{
+    private HashSet<ITickable> _tickables = new HashSet<ITickable>();
+
+    public void Register(ITickable tickable)
+    {
+        _tickables.Add(tickable);
+    }
+
+    private void Update()
+    {
+        foreach (var tickable in _tickables)
+        {
+            tickable.Tick();
+        }
+    }
+}
