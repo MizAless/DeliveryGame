7b4ce085252ae30f53224dace4f1ebeff866467e
Mizaless <44200635+MizAless@users.noreply.github.com>
2025-10-09 19:31:47 +0300

    Начало фоллоу камеры

---
+++ Assets/_Game/Scripts/FollowCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class FollowCamera : ITickable
+{
+    private readonly Camera _camera;
+    private readonly Transform _target;
+
+    private float _distance;
+    private float _verticalAngle;
+    private float _horizontalAngle;
+
+    public FollowCamera(Camera camera, Transform target, FollowCameraData data)
+    {
+        _camera = camera;
+        _target = target;
+        
+        _distance = data.Distance;
+        _verticalAngle = data.VerticalAngle;
+        _horizontalAngle = data.HorizontalAngle;
+    }
+
+    public void Tick()
+    {
+        var offset = Vector3.forward * _distance;  
+        // var direction = Quaternion.Euler(_verticalAngle, _horizontalAngle, 0) * offset;
+        
+        var direction = Quaternion.identity * new Vector3(_horizontalAngle, 0, -_verticalAngle);
+        
+        // direction *= _distance;
+
+        _camera.transform.position = _target.position;
+        _camera.transform.rotation = Quaternion.Euler(direction);
+        
+        
+        _camera.transform.position += Vector3.forward * _distance;
+        _camera.transform.LookAt(_target.position);
+    }
+}
+
+public struct FollowCameraData
+{
+    public float Distance;
+    public float VerticalAngle;
+    public float HorizontalAngle;
+}
--- Assets/_Game/Scripts/GameplaySceneEntryPoint.cs
+++ Assets/_Game/Scripts/GameplaySceneEntryPoint.cs
@@ -6,6 +6,7 @@ public class GameplaySceneEntryPoint : MonoBehaviour
     
     [SerializeField] private Updater _updater;
 
+    [SerializeField] private Camera _camera;
     private Actions _actions;
 
     private void Awake()
@@ -24,7 +25,17 @@ public class GameplaySceneEntryPoint : MonoBehaviour
         IMoveInput moveInput = new PlayerInput(_actions);
         PlayerFactory factory = new PlayerFactory(_playerPrefab, moveInput);
         var player = factory.Create();
+
+        var followCameraData = new FollowCameraData()
+        {
+            Distance = 5,
+            VerticalAngle = 45,
+            HorizontalAngle = 45,
+        };
+        
+        var followCamera = new FollowCamera(_camera, player.transform, followCameraData);
         
         _updater.Register((ITickable)moveInput);
+        _updater.Register(followCamera);
     }
 }
