dfcae4dc297452c230924bd502dd1de103c2a393
Mizaless <44200635+MizAless@users.noreply.github.com>
2026-01-24 14:24:15 +0300

    begin navigation arrow

---
+++ Assets/_Game/Scripts/NavigationArrow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class NavigationArrow : MonoBehaviour, ITickable
+{
+    [SerializeField] private GameObject _arrow;
+    
+    private Camera _camera;
+    
+    private DeliveryObject _deliveryObject; 
+    
+    public void Tick()
+    {
+        transform.right = -(_camera.transform.position - transform.position).normalized;
+        
+        
+        _arrow.transform.forward = transform.position - _deliveryObject.transform.position;
+    }
+}
