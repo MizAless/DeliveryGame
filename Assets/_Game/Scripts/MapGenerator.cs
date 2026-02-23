2e407c19ebf5b30a46083c798df0256e9e858e91
Mizaless <44200635+MizAless@users.noreply.github.com>
2026-02-23 11:00:27 +0300

    MapGenerator begin

---
+++ Assets/_Game/Scripts/MapGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator
+{
+    List<ChunkData> chunks = new List<ChunkData>();
+    
+    public void Generate()
+    {
+        chunks.Clear();
+        
+        
+    }
+    
+}
+
+public class ChunkData
+{
+    public Vector2 Position;
+    public Chunk.Sides ActiveSides;
+}
