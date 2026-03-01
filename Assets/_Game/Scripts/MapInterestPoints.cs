6f57505a12fb626fc22e6928fe6ed933fc920cfb
MizAless <44200635+MizAless@users.noreply.github.com>
2026-03-01 15:18:11 +0300

    begin quest spawn npc and loot on map

---
+++ Assets/_Game/Scripts/MapInterestPoints.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MapInterestPoints
+{
+    MapContainer _mapContainer;
+
+    public MapInterestPoints(MapContainer mapContainer)
+    {
+        _mapContainer = mapContainer;
+    }
+
+    public Vector3 GetRandomNPCSpawnPoint()
+    {
+        var randomIndex = Random.Range(0, _mapContainer.Chunks.Count);
+
+        return _mapContainer.Chunks[randomIndex].GetComponent<SpawnPointsContainer>().DeliveryLootPoint.position;
+    }
+    
+    public Vector3 GetRandomNPCSpawnPoint()
+    {
+        var randomIndex = Random.Range(0, _mapContainer.Chunks.Count);
+
+        return _mapContainer.Chunks[randomIndex].GetComponent<SpawnPointsContainer>().NpcPoint.position;
+    }
+}
---
+++ Assets/_Game/Scripts/SpawnPointsContainer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SpawnPointsContainer : MonoBehaviour
+{
+    [SerializeField] private Transform _npcPoint;
+    [SerializeField] private Transform _deliveryLootPoint;
+    
+    public Transform NpcPoint => _npcPoint;
+    public Transform DeliveryLootPoint => _deliveryLootPoint;
+}
